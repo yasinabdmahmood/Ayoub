99e6b363463d5b7a2ec0b55b00dd26532d221cb3
yasinabdmahmood <abd181103@gmail.com>
2022-10-24 01:25:50 +0300

    initial draft

---
+++ index.js
@@ -0,0 +1,21 @@
+const btn = document.querySelector("#calculate");
+btn.addEventListener('click',calculate)
+
+function calculate() {
+    const numOfAllShares =parseInt(document.querySelector('.total-shares input').value) ;
+    const numOfSingleShare =parseInt(document.querySelector('.single-shares input').value) ;
+    const don = parseInt(document.querySelector('#donem-in').value) ;
+    const olk = parseInt(document.querySelector('#olk-in').value) ;
+    const mtr = parseInt(document.querySelector('#meter-in').value) ;
+    const area = 2500*don+100*olk+mtr;
+    const ratio = numOfSingleShare/numOfAllShares;
+    const a2 = area*ratio;
+    const don2 = Math.floor(a2/2500);
+    const olk2 = Math.floor((a2%2500)/100);
+    const mtr2 = (a2%2500)%100;
+    console.log(don2,olk2,mtr2)
+    document.getElementById('donem-out').innerHTML =don2;
+    document.getElementById('olk-out').innerHTML =olk2;
+    document.getElementById('meter-out').innerHTML =mtr2;
+
+}
